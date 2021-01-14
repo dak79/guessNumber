99f4c4d4e052deffec57b481a08bb51b2ab26a88
dak79 <danielecampari@gmail.com>
2021-01-14 18:19:45 +0300

    js and html. no style yet

---
+++ js/script.js
@@ -0,0 +1,23 @@
+var randomNumber = Math.round(Math.random() * 50);
+console.log(randomNumber);
+
+function check() {
+
+  let numberInput = document.querySelector('#numberInput').value;
+  console.log(numberInput);
+
+  if (numberInput > randomNumber) {
+    console.log('Hint: too big, select a lower number');
+
+  } else if (numberInput < randomNumber) {
+    console.log('Hint: too small, select a bigger number');
+
+  } else if (numberInput == randomNumber) {
+    console.log('Well Done, the random number was ' + randomNumber);
+
+  }
+}
+
+function reloadPage() {
+  window.location.reload();
+}
